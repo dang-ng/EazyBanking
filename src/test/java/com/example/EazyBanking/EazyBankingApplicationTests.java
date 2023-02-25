f6b1f38cab7667fc745c118ebb2eb1b0f88622de
dang-ng <nguyen.hai.dang@icloud.com>
2023-02-25 19:29:33 +0700

    first commit

---
+++ src/test/java/com/example/EazyBanking/EazyBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EazyBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
